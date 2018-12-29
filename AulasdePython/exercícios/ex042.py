76a9256b421f347971c2da638152886edfb39cd2
BielMaxBR <gbielmaximo@gmail.com>
2018-12-29 16:33:56 -0300

    ex042 feito

--- AulasdePython/exercícios/ex042.py
+++ AulasdePython/exercícios/ex042.py
@@ -2,21 +2,15 @@ v1 = float(input('primeiro segmento: '))
 v2 = float(input('segundo segmento: '))
 v3 = float(input('terceiro segmento: '))
 
-if v3 > v2 and v3 > v1:
-    if v1 + v2 > v3:
-        print('dá pra fazer um triângulo :)')
+if v1 + v2 > v3 and v3 + v1 > v2 and v3 + v2 > v1:
+    print('dá pra fazer um triângulo :)')
+    if v1 == v2 == v3:
+        print('seu triângulo é Equilátero')
+    elif v1 == v2 or v1 == v3 or v2 == v3:
+        print('seu triângulo é Isóceles')
     else:
-        print('não dá pra fazer um triângulo :(')
+        print('seu triângulo é Escaleno')
+else:
+    print('não dá pra fazer um triângulo :(')
 
 
-if v2 > v1 and v2 > v3:
-    if v3 + v1 > v2:
-        print('dá pra fazer um triângulo :)')
-    else:
-        print('não dá pra fazer um triângulo :(')
-
-if v1 > v3 and v1 > v2:
-    if v3 + v2 > v1:
-        print('dá pra fazer um triângulo :)')
-    else:
-        print('não dá pra fazer um triângulo :(')

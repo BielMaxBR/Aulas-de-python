fc0c78665c1e700a6af7db9a106c77969a44b1d2
BielMaxBR <gbielmaximo@gmail.com>
2018-12-29 15:46:58 -0300

    transferencia rápida

--- AulasdePython/exercícios/ex035.py
+++ AulasdePython/exercícios/ex035.py
@@ -1,4 +1,3 @@
-from music_player import ntocar
 v1 = float(input('primeiro segmento: '))
 v2 = float(input('segundo segmento: '))
 v3 = float(input('terceiro segmento: '))
@@ -6,10 +5,8 @@ v3 = float(input('terceiro segmento: '))
 if v3 > v2 and v3 > v1:
     if v1 + v2 > v3:
         print('dá pra fazer um triângulo :)')
-        ntocar(r'C:\Users\MAXIMUS\Documents\biel\RhythmDoctorOffline\assets\sounds\sndOrientalDubstep.mp3')
     else:
         print('não dá pra fazer um triângulo :(')
-        ntocar(r'C:\Users\MAXIMUS\Documents\biel\RhythmDoctorOffline\assets\sounds\sndIntimate.mp3')
     """botão de parar"""
     parar = input('escreva algo pra parar')
     if parar == str:
@@ -20,10 +17,8 @@ if v3 > v2 and v3 > v1:
 if v2 > v1 and v2 > v3:
     if v3 + v1 > v2:
         print('dá pra fazer um triângulo :)')
-        ntocar(r'C:\Users\MAXIMUS\Documents\biel\RhythmDoctorOffline\assets\sounds\sndOrientalDubstep.mp3')
     else:
         print('não dá pra fazer um triângulo :(')
-        ntocar(r'C:\Users\MAXIMUS\Documents\biel\RhythmDoctorOffline\assets\sounds\sndIntimate.mp3')
     """botão de parar"""
     parar = input('escreva algo pra parar')
     if parar == str:
@@ -34,10 +29,8 @@ if v2 > v1 and v2 > v3:
 if v1 > v3 and v1 > v2:
     if v3 + v2 > v1:
         print('dá pra fazer um triângulo :)')
-        ntocar(r'C:\Users\MAXIMUS\Documents\biel\RhythmDoctorOffline\assets\sounds\sndOrientalDubstep.mp3')
     else:
         print('não dá pra fazer um triângulo :(')
-        ntocar(r'C:\Users\MAXIMUS\Documents\biel\RhythmDoctorOffline\assets\sounds\sndIntimate.mp3')
     """botão de parar"""
     parar = input('escreva algo pra parar')
     if parar == str:

6bc1696c4a743013f02e7b06fe86eab3ce5490d3
BielMaxBR <gbielmaximo@gmail.com>
2018-12-31 17:44:45 -0300

    Tela do joguinho

--- AulasdePython/joguinho.py
+++ AulasdePython/joguinho.py
@@ -1,13 +1,13 @@
 import pygame
-
-"""a musica
-pygame.mixer.pre_init(48300, -16, 1, 512)
-pygame.mixer.init()
+from pygame.locals import *
+tela = pygame.display.set_mode((200, 100))
+continua = True
+clock = pygame.time.Clock()
 pygame.init()
-pygame.mixer.music.load('big_hero.mp3')
-pygame.mixer.music.play()
-a tela"""
-pygame.display.init()
-pygame.display.set_mode([600, 480])
-"""fim"""
-input()
+while continua:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            continua = False
+    tela.fill((255, 255, 255))
+    pygame.display.flip()

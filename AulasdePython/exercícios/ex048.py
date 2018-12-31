60b034549ae35aee61c9a204d1ac9987232136a6
BielMaxBR <gbielmaximo@gmail.com>
2018-12-31 16:34:01 -0300

    ex048 feito

--- AulasdePython/exercícios/ex048.py
+++ AulasdePython/exercícios/ex048.py
@@ -1,4 +1,7 @@
-
-''''for c in range(1, 500, 3):
-    if (c / c * 3) == 3:
-        print(c''
+total = 0
+valores = 0
+for c in range(1, 500, 2):
+    if c % 3 == 0:
+        total = total + c
+        valores += 1
+print('a soma de todos os {} valores é {}'.format(valores, total))

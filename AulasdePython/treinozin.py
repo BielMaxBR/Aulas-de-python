a3fdbface5c3c7ccd4c01530457ab3090143a964
BielMaxBR <gbielmaximo@gmail.com>
2019-01-01 19:31:15 -0300

    Treino de funções

--- AulasdePython/treinozin.py
+++ AulasdePython/treinozin.py
@@ -1,2 +1,9 @@
 # todos os teste de coisas que eu aprendo ficam aqui
+class imprecao():
+    def print_oi():
+        print('Oi')
 
+    def print_chal():
+        print('chal')
+
+imprecao.print_oi()

e0c631bd8fd89f093fc3dac6e7d6fb0b6e043661
BielMaxBR <gbielmaximo@gmail.com>
2019-01-01 19:39:36 -0300

    Função Relógio

---
+++ AulasdePython/Relógio.py
@@ -0,0 +1,17 @@
+def clock():
+    from datetime import datetime
+    import sys
+    t = datetime.now()
+    h = t.hour
+    m = t.minute
+    s = t.second
+
+    while True:
+        try:
+            sys.stdout.write('\r{}:{}:{}'.format(h, m, s))
+            sys.stdout.flush()
+            h = int(datetime.now().hour)
+            m = int(datetime.now().minute)
+            s = int(datetime.now().second)
+        except KeyboardInterrupt as e:
+            break

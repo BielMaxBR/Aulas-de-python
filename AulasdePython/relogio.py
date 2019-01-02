39f24f1f622894fdc451e9d2fe04d77f6fc47947
BielMaxBR <gbielmaximo@gmail.com>
2019-01-02 18:21:32 -0300

    Relógio FINALMENTE funcionando

--- AulasdePython/relogio.py
+++ AulasdePython/relogio.py
@@ -1,18 +1,21 @@
-class relogio:
-    def clock():
-        from datetime import datetime
-        import sys
-        t = datetime.now()
-        h = t.hour
-        m = t.minute
-        s = t.second
+def clock():
+    from datetime import datetime
+    import sys
+    t = datetime.now()
+    h = t.hour
+    m = t.minute
+    s = t.second
 
-        while True:
-            try:
-                sys.stdout.write('\r{}:{}:{}'.format(h, m, s))
-                sys.stdout.flush()
-                h = int(datetime.now().hour)
-                m = int(datetime.now().minute)
-                s = int(datetime.now().second)
-            except KeyboardInterrupt as e:
-                break
+    while True:
+        try:
+            sys.stdout.write('\r{}:{}:{}'.format(h, m, s))
+            sys.stdout.flush()
+            h = int(datetime.now().hour)
+            m = int(datetime.now().minute)
+            s = int(datetime.now().second)
+        except KeyboardInterrupt as B:
+            break
+
+
+class Relogio:
+    pass
--- AulasdePython/treinozin.py
+++ AulasdePython/treinozin.py
@@ -1,10 +1,1 @@
 # todos os teste de coisas que eu aprendo ficam aqui
-
-class imprecao():
-    def print_oi():
-        print('Oi')
-
-    def print_chal():
-        print('chal')
-
-imprecao.print_oi()

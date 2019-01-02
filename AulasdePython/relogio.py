6987f07f34b7ce4fae0a94f2a346b0a43aa0c3cb
BielMaxBR <gbielmaximo@gmail.com>
2019-01-02 18:03:56 -0300

    Relógio em andamento

--- AulasdePython/relogio.py
+++ AulasdePython/relogio.py
@@ -1,20 +1,18 @@
-def clock():
-    from datetime import datetime
-    import sys
-    t = datetime.now()
-    h = t.hour
-    m = t.minute
-    s = t.second
+class relogio:
+    def clock():
+        from datetime import datetime
+        import sys
+        t = datetime.now()
+        h = t.hour
+        m = t.minute
+        s = t.second
 
-    while True:
-        try:
-            sys.stdout.write('\r{}:{}:{}'.format(h, m, s))
-            sys.stdout.flush()
-            h = int(datetime.now().hour)
-            m = int(datetime.now().minute)
-            s = int(datetime.now().second)
-        except KeyboardInterrupt as e:
-            break
-
-
-
+        while True:
+            try:
+                sys.stdout.write('\r{}:{}:{}'.format(h, m, s))
+                sys.stdout.flush()
+                h = int(datetime.now().hour)
+                m = int(datetime.now().minute)
+                s = int(datetime.now().second)
+            except KeyboardInterrupt as e:
+                break

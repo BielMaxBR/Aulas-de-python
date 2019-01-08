b68af5f62d91f7b08776e8f54d291b42db9534d2
BielMaxBR <gbielmaximo@gmail.com>
2019-01-08 16:35:57 -0300

    bot do dino em andamento

--- AulasdePython/treinozin.py
+++ AulasdePython/treinozin.py
@@ -1,24 +1,45 @@
 # todos os teste de coisas que eu aprendo ficam aqui
-from datetime import datetime
-import sys
-import music_player
-t = datetime.now()
-h = t.hour
-m = t.minute
-s = t.second
+from PIL import ImageGrab
+import time
 
-while True:
-    try:
-        sys.stdout.write('\r{}:{}:{}'.format(h, m, s))
-        sys.stdout.flush()
-        h = int(datetime.now().hour)
-        m = int(datetime.now().minute)
-        s = int(datetime.now().second)
-    except KeyboardInterrupt:
-        print('\nRelógio desligado')
-        break
-    if h == 15 and m == 0:
-        print('\nhora de lanchar')
-        music_player.rtocar('pvz.mp3.mp3')
-        break
+try:
+    import pyautogui
+except Exception as err:
+    print('erro no módulo:{}'.format(err))
 
+x = None
+y = None
+
+def Coletor_de_tela():
+    tela = ImageGrab.grab()
+    return tela
+
+
+def Detector_de_inimigo(Tela):
+    #if x in range(310, 338):
+    #    if y in range(463, 340):
+    color = Tela.getpixel((338, 463))
+    if color == (83, 83, 83):
+        return True
+
+
+def pule():
+    pyautogui.press('up')
+
+
+try:
+    print('espere 3 segundos...')
+    time.sleep(3)
+    while True:
+        try:
+            screen = Coletor_de_tela()
+            if Detector_de_inimigo(screen) is True:
+                print('pulou')
+                pule()
+
+        except Exception as err:
+            print('deu ruim', err)
+            break
+
+except KeyboardInterrupt:
+    print('Encerrado')

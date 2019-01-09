acafe17f9257adfb424e6d870da7f69c0443699f
BielMaxBR <gbielmaximo@gmail.com>
2019-01-09 16:46:01 -0300

    treino de portscan (até a porta 199)

--- AulasdePython/treinozin.py
+++ AulasdePython/treinozin.py
@@ -1,45 +1,42 @@
 # todos os teste de coisas que eu aprendo ficam aqui
-from PIL import ImageGrab
-import time
+import socket
 
 try:
-    import pyautogui
+    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    PORTS = 0
+    contagem = 0
+    porta = [0,1,2,3,4,5,6,7,8,9,
+             10,11,12,13,14,15,16,17,18,19,
+             20,21,22,23,24,25,26,27,28,29,
+             30,31,32,33,34,35,36,37,38,39,
+             40,41,42,43,44,45,46,47,48,49,
+             50,51,52,53,54,55,56,57,58,59,
+             60,61,62,63,64,65,66,67,68,69,
+             70,71,72,73,74,75,76,77,78,79,
+             80,81,82,83,84,85,86,87,88,89,
+             90,91,92,93,94,95,96,97,98,99,
+             100, 101, 102, 103, 104, 105, 106, 107, 108, 109,
+             110, 111, 112, 113, 114, 115, 116, 117, 118, 119,
+             120, 121, 122, 123, 124, 125, 126, 127, 128, 129,
+             130, 131, 132, 133, 134, 135, 136, 137, 138, 139,
+             140, 141, 142, 143, 144, 145, 146, 147, 148, 149,
+             150, 151, 152, 153, 154, 155, 156, 157, 158, 159,
+             160, 161, 162, 163, 164, 165, 166, 167, 168, 169,
+             170, 171, 172, 173, 174, 175, 176, 177, 178, 179,
+             180, 181, 182, 183, 184, 185, 186, 187, 188, 189,
+             190, 191, 192, 193, 194, 195, 196, 197, 198, 199,
+             ]
+
+    print('total conectado | resposta | Porta atual')
+
+    for portas in porta:
+        resposta = cliente.connect_ex(('bancocn.com', porta))
+        cliente.settimeout(5)
+
+        if resposta == 0:
+            contagem += 1
+
+        print('{} {} {}'.format(contagem, resposta, PORTS))
 except Exception as err:
-    print('erro no módulo:{}'.format(err))
+    print(err)
 
-x = None
-y = None
-
-def Coletor_de_tela():
-    tela = ImageGrab.grab()
-    return tela
-
-
-def Detector_de_inimigo(Tela):
-    #if x in range(310, 338):
-    #    if y in range(463, 340):
-    color = Tela.getpixel((338, 463))
-    if color == (83, 83, 83):
-        return True
-
-
-def pule():
-    pyautogui.press('up')
-
-
-try:
-    print('espere 3 segundos...')
-    time.sleep(3)
-    while True:
-        try:
-            screen = Coletor_de_tela()
-            if Detector_de_inimigo(screen) is True:
-                print('pulou')
-                pule()
-
-        except Exception as err:
-            print('deu ruim', err)
-            break
-
-except KeyboardInterrupt:
-    print('Encerrado')

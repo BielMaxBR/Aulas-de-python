6b2ef0bbbc0638bbbbba0598a0a5e7629007298e
BielMaxBR <gbielmaximo@gmail.com>
2019-01-10 14:41:47 -0300

    Kivy treino 1 (errado)

---
+++ AulasdePython/kivy_treinos.py
@@ -0,0 +1,30 @@
+import kivy
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+
+kivy.require('1.10.1')
+
+
+class HAHAHAHAHA(GridLayout):
+    def __init__(self):
+        super(HAHAHAHAHA, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(Label(text='HAHAHAHA'))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
+        self.add_widget(Label(text='HAHA'))
+        self.password = TextInput(password=True, multiline=False)
+        self.add_widget(self.password)
+
+
+class Myapple(App):
+    def build(self):
+        return HAHAHAHAHA()
+
+
+if __name__ == '__main__':
+    Myapple().run()
+
+

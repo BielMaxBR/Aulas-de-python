9a5a6cfbce5104ca3bce84ba50ba34be520b27c9
Gabriel Maximo <gbielmaximo@gmail.com>
2021-08-23 20:47:17 +0000

    z

--- AulasdePython/quiz.py
+++ AulasdePython/quiz.py
@@ -1,4 +1,7 @@
+from colorama import Fore, Style
+
 perguntas = []
+
 
 def criarPergunta(texto: str, certa: int, *alternativas: str):
     pergunta = {
@@ -6,15 +9,18 @@ def criarPergunta(texto: str, certa: int, *alternativas: str):
         'certa': certa,
         'alternativas': alternativas
     }
-    
+
     perguntas.append(pergunta)
 
 
 def iniciarQuiz():
-    for pergunta in perguntas:
+    print(Fore.GREEN+"Iniciando quiz:"+Style.RESET_ALL)
+    for index in range(0, len(perguntas)):
+        print(Fore.YELLOW+"Pergunta número")
 
-criarPergunta("3",3,"1","2","3","4")
-criarPergunta("3",3,"1","2","3","4")
-criarPergunta("3",3,"1","2","3","4")
+
+criarPergunta("3", 3, "1", "2", "3", "4")
+criarPergunta("3", 3, "1", "2", "3", "4")
+criarPergunta("3", 3, "1", "2", "3", "4")
 
 iniciarQuiz()
